60090925ed1ea225204ccf7e15b5ed2470c982d3
emritcey <emma.ritcey@spr.com>
2020-07-27 00:01:44 -0500

    Add functionality to have multiple balls

--- src/Ball/Ball.js
+++ src/Ball/Ball.js
@@ -1,34 +1,115 @@
-import React from 'react';
-
-export default (props) => {
-	const defaultProps = {
+class Ball {
+	
+	constructor(props) {
+		this.canvas = props.canvas;
+		this.x = props.x;
+		this.y = props.y;
+		this.velX = props.velX;
+		this.velY = props.velY;
+		this.bouncing = props.bouncing;
+		this.color = this.getRandomColor();
+		this.radius = this.getRandomRadius();
+		this.bounce = this.defaultProps.bounce;
+		this.velX = this.defaultProps.startVelX;
+		this.velY = this.defaultProps.startVelY;
+	}
+	
+	defaultProps = {
 		bounce: 0.75,
-		radius: 30,
-		color: 'red',
-
-		// starting velocity
 		startVelX: (Math.random() * 15 + 5) * (Math.floor(Math.random() * 2) || -1),
 		startVelY: (Math.random() * 15 + 5) * (Math.floor(Math.random() * 2) || -1)
 	}
 
-	const draw = (ctx) => {
+	gravity = 0.25;
+	friction = 0.98;
+
+	draw (ctx) {
+		ctx.save();
 		ctx.beginPath();
 		ctx.arc(
-			props.details.x,
-			props.details.y,
-			props.details.radius,
+			this.x,
+			this.y,
+			this.radius,
 			0,
 			Math.PI * 2
 		);
 		ctx.closePath();
-		ctx.fillStyle = "skyblue";
+		ctx.fillStyle = this.color;
 		ctx.fill();
+		ctx.restore();
 	}
 
-	const update = (ctx) => {
+	update () {
+		// handle collisions
+		this.handleCeiling();
+		this.handleFloor();
+		this.handleLeft();
+		this.handleRight();
 
+		// handle if the ball stops bouncing
+		if (this.velX < 0.01 && this.velX > -0.01) {
+			this.velX = 0;
+			this.bouncing = false;
+		}
+		if (this.velY < 0.01 && this.velY > -0.01) {
+			this.velY = 0;
+			this.y = this.canvas.height - this.radius;
+		}
+
+		// account for gravity
+		if (this.velY !== 0) {
+			this.velY += this.gravity;
+			this.y += this.velY;
+		}
+
+		// update this position
+		this.x += this.velX;
 	}
 
+	 handleCeiling () {
+		if (this.y - this.radius <= 0) {
+			this.velY *= -this.bounce
+			this.y = this.radius
+			this.velX *= this.friction
+		}
+	}
 
+	handleFloor () {
+		if (this.y + this.radius >= this.canvas.height) {
+			this.velY *= -this.bounce;
+			this.y = this.canvas.height - this.radius;
+			this.velX *= this.friction;
+		}
+	}
 
+	handleRight () {
+		if (this.x + this.radius >= this.canvas.width) {
+			this.velX *= -this.bounce;
+			this.x = this.canvas.width - this.radius;
+		}
+	}
+
+	handleLeft () {
+		if (this.x - this.radius <= 0) {
+			this.velX *= -this.bounce;
+			this.x = this.radius;
+		}
+	}
+
+	getRandomColor () {
+		const letters = '0123456789ABCDEF';
+		let color = '#';
+		for (let i = 0; i < 6; i++) {
+			color += letters[Math.floor(Math.random() * 16)];
+		}
+		return color;
+	}
+
+	getRandomRadius () {
+		const min = 20;
+		const max = 70;
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
 }
+
+export default Ball;
--- src/BallPit/BallPit.js
+++ src/BallPit/BallPit.js
@@ -1,12 +1,12 @@
 import React, {useEffect, useRef} from 'react';
 import './BallPit.scss';
+import Ball from "../Ball/Ball";
 
 export default () => {
     let canvas, ctx, ball, requestRef;
     canvas = useRef(null);
 
-    const gravity = 0.25;
-    const friction = 0.98;
+    let balls = [];
 
     useEffect(() => {
         init();
@@ -16,119 +16,65 @@ export default () => {
 
     const init = () => {
         if (canvas.current) {
-            ctx = canvas.current.getContext('2d');
+            canvas = canvas.current;
+            ctx = canvas.getContext('2d');
         }
-
-        ball = {
-            bounce: 0.9, // energy lost on bounce (25%)
-            radius: 30,
-            x: canvas.current.width / 2,
-            y: canvas.current.height / 2,
-            velX: (Math.random() * 15 + 5) * (Math.floor(Math.random() * 2) || -1),
-            velY: (Math.random() * 15 + 5) * (Math.floor(Math.random() * 2) || -1)
-        }
-        console.log('init')
     }
     const update = () => {
-        // queue the next update
-        requestRef = requestAnimationFrame(() => update());
-        console.log('update');
-        // bottom bound / floor
-        if (ball.y + ball.radius >= canvas.current.height) {
-            ball.velY *= -ball.bounce;
-            ball.y = canvas.current.height - ball.radius;
-            ball.velX *= friction;
-        }
-        // top bound / ceiling
-        if (ball.y - ball.radius <= 0) {
-            ball.velY *= -ball.bounce
-            ball.y = ball.radius
-            ball.velX *= friction
+        clearScreen();
+        // only update if there are active balls
+        if (balls.length > 0) {
+            requestRef = requestAnimationFrame(() => update());
         }
 
-        // left bound
-        if (ball.x - ball.radius <= 0) {
-            ball.velX *= -ball.bounce;
-            ball.x = ball.radius;
-        }
-        // right bound
-        if (ball.x + ball.radius >= canvas.current.width) {
-            ball.velX *= -ball.bounce;
-            ball.x = canvas.current.width - ball.radius;
-        }
+        // update each ball if it is bouncing
+        balls.forEach(ball => {
+            if (ball.bouncing) {
+                ball.update();
+            } else {
+                clearScreen();
+                return () => cancelAnimationFrame(requestRef.current);
+            }
+        });
 
-        // reset insignificant amounts to 0
-        if (ball.velX < 0.01 && ball.velX > -0.01) {
-            ball.velX = 0
-        }
-        if (ball.velY < 0.01 && ball.velY > -0.01) {
-            ball.velY = 0;
-        }
+        // draw each ball
+        balls.forEach(ball => ball.draw(ctx));
 
-        // add gravity
-        ball.velY += gravity;
-
-        // update ball position
-        ball.x += ball.velX;
-        ball.y += ball.velY;
-
-        // draw after logic/calculations
-        draw();
-
+        // remove balls from array if they are done bouncing
+        balls = balls.filter(ball => ball.bouncing && ball.y > 0);
     }
 
     const clearScreen = () => {
         ctx.clearRect(
           0,
           0,
-          canvas.current.width,
-          canvas.current.height)
+          canvas.width,
+          canvas.height)
     }
 
-    const draw = () => {
-        if (ctx) {
-            clearScreen();
-
-            // draw the ball (only object in this scene)
-            ctx.beginPath();
-            ctx.fillStyle = 'skyblue';
-
-            ctx.arc(
-                ball.x,
-                ball.y,
-                ball.radius,
-                0,
-              Math.PI * 2
-            )
-            ctx.closePath();
-
-            ctx.fill()
-
-        }
-    }
-
-    const moveBall = (e) => {
-        console.log(e.clientX, e.clientY);
-        // ball.x = e.clientX;
-        // ball.y = e.clientY;
-        ball = {
-            bounce: 0.75, // energy lost on bounce (25%)
-            radius: 30,
+    const addBall = (e) => {
+        let ball = new Ball({
             x: e.clientX,
             y: e.clientY,
-            velX: (Math.random() * 15 + 5) * (Math.floor(Math.random() * 2) || -1),
-            velY: (Math.random() * 15 + 5) * (Math.floor(Math.random() * 2) || -1)
-        }
-        console.log(ball);
-        draw();
-        // draw();
-    }
+            bouncing: true,
+            canvas: canvas,
+        });
 
+        if (balls.length === 0) {
+            balls.push(ball);
+            update();
+        } else {
+            balls.push(ball);
+        }
+
+        ball.update();
+        ball.draw(ctx);
+    }
 
     return (
         <canvas ref={canvas}
                 width={window.innerWidth}
                 height={window.innerHeight}
-                onClick={moveBall} />
+                onClick={addBall} />
     );
 }
